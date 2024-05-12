eb3e01ae011c12167a3cd9495521badb71c78dc2
eerotuo <eero.tuomaala@hotmail.com>
2024-05-12 20:46:33 +0300

    New action

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,33 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module' // Set sourceType directly here
   },
-  "plugins": [
-    "react", "jest"
+  'plugins': [
+    '@stylistic/js'
   ],
-  "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
+  'extends': 'eslint:recommended',
+  'rules': {
+    '@stylistic/js/indent': [
+      'error',
+      2
     ],
-    "quotes": [
-      "error",
-      "single"
+    '@stylistic/js/linebreak-style': [
+      'error',
+      'unix'
     ],
-    "semi": [
-      "error",
-      "never"
+    '@stylistic/js/quotes': [
+      'error',
+      'single'
     ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
+    '@stylistic/js/semi': [
+      'error',
+      'never'
     ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
   }
 }
